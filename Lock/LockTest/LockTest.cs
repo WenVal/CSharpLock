e94e1fea50d43a4c5bd741ff8c71b0e21bcdf05a
Val_Wen <38552157+WenVal@users.noreply.github.com>
2018-04-21 13:48:05 +0800

    Add files via upload

---
+++ Lock/LockTest/LockTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockTest
+{
+    public class LockTest<T>
+    {
+        private IList<T> list;
+        /// <summary>
+        /// 构建锁
+        /// </summary>
+        private Object locker = new Object();
+        public LockTest()
+        {
+            this.list = new List<T>();
+
+
+        }
+        public void Add(T t)
+        {
+
+            lock (locker)
+            {
+
+                list.Add(t);
+            }
+        }
+
+        public int Remove(T t)
+        {
+            if (list.Contains<T>(t))
+            {
+                lock (locker)
+                {
+                    list.Remove(t);
+                }
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+
+        }
+
+    }
+}
